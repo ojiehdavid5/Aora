7f472008f84aa47e9062d2b069cedf8f770e1aeb
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-04 17:10:33 +0100

    changes

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,54 +1,104 @@
-import { View, Text,Image } from 'react-native'
-import React from 'react'
-import {Tabs,Redirect} from 'expo-router';
-import {icons} from '../../constants';
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { icons } from '../../constants';
 
-const TabIcon=({ icon,color,name,focused })=>{
-  return(
-    <View className="justify-center items-center gap-20">
-      <Image source={icon}
-      resizeMode="contain"
-      tintColor={color}
-      className="w-6  h-6"
-      
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="justify-center items-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
       />
-      <Text >{name}</Text>
-
+      <Text style={{color:color}}>{name}</Text>
     </View>
-  )
-}
+  );
+};
 
 const TabsLayout = () => {
   return (
-   <>
-   <Tabs
-   screenOptions={{
-    tabBarShowLabel:false
-   }}
-   
-   >
-    <Tabs.Screen
-     name='home'
-     options={{
-        title: 'Home',
-        headerShown:false,
-        tabBarIcon:({ color,focused})=>(
-<TabIcon
-icon={icons.home}
-color={color}
-name="Home"
-focused={focused}
-className="w-6 h-6"
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor:'#FFA001',
+          tabBarInactiveTintColor:'#CDCDE0',
+          tabBarStyle:{
+            backgroundColor:'#161622',
+            borderTopWidth:1,
+            borderTopColor:'#232533',
+            height:84
+          }
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: 'Bookmark',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name="Bookmark"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-/>
-        )
-     }}
-    
-    />
-   </Tabs>
-   </>
-  )
-}
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: 'Create',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name="Create"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-export default TabsLayout
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile} // Ensure this is the correct reference
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,63 @@
-import { StyleSheet, Text, View ,StatusBar} from 'react-native'
+import { StyleSheet, Text, View ,StatusBar,SafeAreaView, ScrollView,Image} from 'react-native'
 import React from 'react'
 import {Slot,Link} from "expo-router"
+import { images}   from "../constants/";
+import CustomButton from "../components/CustomButton"
+import {Redirect ,router} from 'expo-router';
 
 
 
 const RootLayout = () => {
   return (
-    <>
-    <View style={styles.container}>
 
-    <Text className='text-5xl '>Aora</Text>
-    <StatusBar style="auto"/>
-    <Link href="/home"> 
-    Go to home
-    
-    </Link>
+   
 
         
-    </View>
+<SafeAreaView className="bg-primary h-full">
+  <ScrollView contentContainerStyle={{height: '100%'}}>
+    <View className='w-full items-center  h-full px-4'>
+<Image 
+source={images.logo}
+className="w-[130px] h-[84px]"
+resizeMode='contain'
 
-    </>
+/>
+<Image
+source={images.cards}
+className="max-w-[380px] w-full h-[300px]"
+resizeMode='contain'
+
+/>
+
+<View className='relative mt-9'>
+  <Text className='text-4xl text-white font-bold  text-center'>
+Discover Endless Possiblities  With{' '} <Text className='text-secondary-200'>Aora</Text>
+  </Text>
+  <Image 
+  source={images.path}
+  className='w-[136px] h-[15px] absolute  -bottom-2 -right-2'
+  resizeMode='contain'
+  
+  />
+
+</View>
+
+<Text className='text-center  text-gray-100  mt-7  text-sm font-pregular'> Where creativity meets innovation:embark on a journey of limitless exploration  with Aora</Text>
+
+<CustomButton
+title='Continue With Email'
+handlePress={()=>router.push('./sign-in')}
+containerStyles='w-full mt-7'
+
+/>
+    </View>
+  </ScrollView>
+  <StatusBar
+  backgroundColor='#161622'
+  style='dark'
+  />
+    
+</SafeAreaView>
 
   )
   
