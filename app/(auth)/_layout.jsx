60e8a5e6c571dad77f66554e796482f9064787eb
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-04 17:22:34 +0100

    changes

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text,SafeAreaView,ScrollView,Image } from 'react-native'
 import React from 'react'
+import {images} from '../../constants'
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text> AuthLayout</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='justify-center w-full  h-full px-4 my-6'>
+          <Image
+          source={images.logo}
+          resizeMode='contain'
+          className='w-[115px] h-[35px]'
+          
+          />
+          <Text className='text-2xl text-white text-semibold mt-10 font-psemibold'>
+            Log in to Aora
+          </Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
