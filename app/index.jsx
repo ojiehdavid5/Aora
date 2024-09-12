117daeb3841b450f65dfda826fb44aa7edffa4ec
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-12 15:08:09 +0100

    DONE

--- app/index.jsx
+++ app/index.jsx
@@ -4,10 +4,15 @@ import {Slot,Link} from "expo-router"
 import { images}   from "../constants/";
 import CustomButton from "../components/CustomButton"
 import {Redirect ,router} from 'expo-router';
+import { useGlobalContext } from '../context/GlobalProvider'
 
 
 
 const RootLayout = () => {
+
+  const {isLoading,isLoggedIn}=useGlobalContext();
+  if(!isLoading && isLoggedIn) return <Redirect href='/home'/>
+
   return (
 
    
--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,17 +1,46 @@
 import {createContext,useState,useEffect,useContext} from 'react';
-const GlobalContext =createContext;
+const GlobalContext =createContext();
+import { getCurrentUser } from '../lib/appwrite';
 
 
 export const useGlobalContext=()=>useContext(GlobalContext)
 
 
 
-const GlobalProvider=({children})=>{
+  const GlobalProvider=({children})=>{
     const [isLoggedIn,setIsLoggedIn]=useState(false);
     const [user,setUser]=useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        getCurrentUser().then((res)=>{
+            if(res){
+                setIsLoggedIn(true);
+                setUser(res);
+                console.log(res);
+            }else{
+                setIsLoggedIn(false);
+                setUser(null);
+
+            }
+        }).catch((err)=>{
+            console.log(err);
+        }).finally(()=>{
+            setIsLoading(false)
+
+        })
+        
+    
+      
+    }, [])
+    
     return(
         <GlobalContext.Provider
         value={{
+            isLoggedIn,
+            isLoading,
+            user,
+            setUser,
+            setIsLoggedIn
 
         }}
         
@@ -20,3 +49,4 @@ const GlobalProvider=({children})=>{
         </GlobalContext.Provider>
     )
 }
+export default GlobalProvider;
