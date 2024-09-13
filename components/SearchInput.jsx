73d92db218e2711771c9be1a418f3a3dd42aaed9
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-13 03:47:59 +0100

    done

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -4,35 +4,38 @@ import React,{useState} from 'react'
 import {icons} from '../constants'
 
 
-const FormField = ({title,value,placeholder,handleChangeText,otherStyles,...props}) => {
+const SearchInput = ({title,value,placeholder,handleChangeText,otherStyles,...props}) => {
   const [showPassword,setShowPassword] =useState(false);
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
-      <View className='border-2 border-red-500 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary  items-center flex-row space-x-4'>
+      <View className='border-2 border-red-500 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary  items-center flex-row'>
 <TextInput
-className='flex-1 text-white font-pregular text-base mt-0.5'
+className='flex-1 text-white font-psemibold text-base'
 value={value}
-placeholder={placeholder}
+placeholder='search for a video topic'
 placeholderTextColor='#7b7b8b'
 onChangeText={handleChangeText}
 secureTextEntry={title==='Password' && !showPassword}
 />
 {title === 'Password' && (
 
+ 
+  <TouchableOpacity >
+  <Image
+  source={icons.eye}
+  className='w-5- h-5'
+  resizeMode='contain'
   
-   <TouchableOpacity onPress={()=>setShowPassword(!showPassword)}>
-   <Image source={!showPassword ? icons.eye : icons.eyeHide}
-   className='w-6 h-6'
-   resizeMode='contain'
-   />
-
- </TouchableOpacity>
+  />
+</TouchableOpacity>
 
 )}
+
+
       </View>
     </View>
   )
 }
 
-export default FormField
+export default SearchInput;
