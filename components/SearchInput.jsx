7e8ffc67f1cb3fc95eb7f6102a2f61f81632e560
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-16 00:07:42 +0100

    done

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -15,11 +15,11 @@ const SearchInput = ({title,value,placeholder,handleChangeText,otherStyles,...pr
   const [showPassword,setShowPassword] =useState(false);
   return (
     <View className={`space-y-2 ${otherStyles}`}>
-      <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
+      <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>  
 
       <View className='border-2 border-red-500 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary  items-center flex-row'>
 <TextInput
-className='flex-1 text-white font-psemibold text-base'
+className='flex-1 text-white font-psemibold text-base'      
 value={value}
 placeholder='search for a video topic'
 placeholderTextColor='#7b7b8b'
