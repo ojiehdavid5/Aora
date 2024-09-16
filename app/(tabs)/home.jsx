538ee4107dc04daf9233b0535e1ecb2e3b1d4564
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-16 01:27:46 +0100

    done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import {images} from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
-import {getAllPosts} from '../../lib/appwrite'
+import {getAllPosts,getLatestPosts} from '../../lib/appwrite'
 // import { RefreshControl } from 'react-native-gesture-handler'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
@@ -12,6 +12,7 @@ import VideoCard from '../../components/VideoCard'
 const Home = () => {
  
   const {data:posts,refetch}=useAppwrite(getAllPosts)
+  const {data:latestPosts}=useAppwrite(getLatestPosts)
 
 
   const [refreshing, setRefreshing] = useState(false);
@@ -20,6 +21,7 @@ const Home = () => {
     await refetch;
 
     setRefreshing(false);
+
 
   }
   return (
@@ -59,7 +61,7 @@ const Home = () => {
 
           </Text>
 
-          <Trending  posts={[{id:1},{id:2},{id:3},{id:2},{id:7}]}/>
+          <Trending  posts={latestPosts}/>
 
         </View>
           
