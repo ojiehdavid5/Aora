2a97e807a097fe1d5d8628d732ec3c64758e7cf1
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-19 18:33:03 +0100

    done

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,39 +1,52 @@
-import { View, Text,TextInput,Image ,TouchableOpacity} from 'react-native'
+import { View, Text,TextInput,Image ,TouchableOpacity, Alert} from 'react-native'
 import React,{useState} from 'react'
 import { icons } from '../constants/';
+import { usePathname,router } from 'expo-router';
 
 
 const SearchInput = ({title,value,placeholder,handleChangeText,otherStyles,...props}) => {
+  
 
 
 
 
 
 
+const pathname=usePathname( );
+const [query, setQuery] = useState('');
 
-
-  const [showPassword,setShowPassword] =useState(false);
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>  
 
       <View className='border-2 border-red-500 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary  items-center flex-row'>
 <TextInput
-className='flex-1 text-white font-psemibold text-base'      
-value={value}
+className='flex-1 text-white font-pregular text-base mt-0.5'      
+value={query}
 placeholder='search for a video topic'
-placeholderTextColor='#7b7b8b'
-onChangeText={handleChangeText}
+placeholderTextColor='#CDCDE0'
+onChangeText={(e)=>setQuery(e)}
 secureTextEntry={title==='Password' && !showPassword}
 
 />
-{title === 'Password' && (
 
  
-  <TouchableOpacity >
+  <TouchableOpacity
+  onPress={() => {
+    if (!query) {
+        return Alert.alert('Missing query', 'Please input something');
+    }
+
+    if (pathname.startsWith('/search')) {
+        router.setParams({ query });
+    } else {
+        router.push(`/search/${query}`);
+    }
+}}
+  >
   <Image
-  source={icons.upload}
-  className='w-5 h-[3rem] ml-10'
+  source={icons.search}
+  className='w-5 h-5'
   resizeMode='contain'
   
   />
@@ -43,7 +56,6 @@ secureTextEntry={title==='Password' && !showPassword}
 
 
 
-)}
 
 
 
