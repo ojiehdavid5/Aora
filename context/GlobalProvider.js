1208294c7e183afa5ae7dc08e48e9ba30b9b5863
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-19 03:40:01 +0100

    done

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -45,7 +45,11 @@ export const useGlobalContext=()=>useContext(GlobalContext)
         }}
         
         >
+            
+
             {children}
+
+
         </GlobalContext.Provider>
     )
 }
