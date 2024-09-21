c39bda6e26bc1edfdb95cf40b8ea95cb36788aab
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-21 23:25:53 +0100

    import { View, Text,SafeAreaView,FlatList,Image,RefreshControl, Alert } from 'react-native' import React,{useState,useEffect} from 'react' import {images} from '../../constants' import {icons} from '../../constants' import SearchInput from '../../components/SearchInput' // import Trending from '../../components/Trending' import EmptyState from '../../components/EmptyState' import {searchPosts} from '../../lib/appwrite' // import { RefreshControl } from 'react-native-gesture-handler' import useAppwrite from '../../lib/useAppwrite' import VideoCard from '../../components/VideoCard' import { useLocalSearchParams } from 'expo-router'
    const Search = () => {
      const {query} = useLocalSearchParams();
    
      // const {data:posts,refetch}=useAppwrite(() => searchPosts(query));
      const {data:posts,refetch} = useAppwrite(()=>searchPosts(query))
    
      console.log(query,posts);
      useEffect(() => {
        refetch();
    
      }, [query])
    
      return (
        <SafeAreaView className='bg-primary h-full'>
          <FlatList
          data={posts}
          // data={[]}
          keyExtractor={(item)=>item.$id}
          renderItem={({item})=>(
            <VideoCard video={item}/>
    
          )}
          ListHeaderComponent={()=>(
            <View className='my-6 px-4 '>
            <Text className='font-pmedium text-sm text-gray-100'>Search Result</Text>
            <Text className='text-2xl  font-psemibold text-white'>{query}</Text>
    
            <View className='mt-6 mb-8'>
            <SearchInput  initialQuery={query}/>
    
            </View>
    
            </View>
          )}
    
          ListEmptyComponent={()=>(
            <EmptyState
            title='No Videos  Found'
            subtitle='No videos found for this query'
    
            />
    
          )}
    
          />
          <Text> Home</Text>
        </SafeAreaView>
      )
    }
    
    export default Search

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -3,9 +3,9 @@ import React,{useState,useEffect} from 'react'
 import {images} from '../../constants'
 import {icons} from '../../constants'
 import SearchInput from '../../components/SearchInput'
-import Trending from '../../components/Trending'
+// import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
-import {getAllPosts,getLatestPosts,searchPosts} from '../../lib/appwrite'
+import {searchPosts} from '../../lib/appwrite'
 // import { RefreshControl } from 'react-native-gesture-handler'
 import useAppwrite from '../../lib/useAppwrite'
 import VideoCard from '../../components/VideoCard'
@@ -15,7 +15,10 @@ import { useLocalSearchParams } from 'expo-router'
 const Search = () => {
   const {query} = useLocalSearchParams();
  
-  const {data:posts,refetch}=useAppwrite(searchPosts(query));
+  // const {data:posts,refetch}=useAppwrite(() => searchPosts(query));
+  const {data:posts,refetch} = useAppwrite(()=>searchPosts(query))
+
+  console.log(query,posts);
   useEffect(() => {
     refetch();
   
@@ -35,27 +38,20 @@ const Search = () => {
 
       )}
       ListHeaderComponent={()=>(
-        <View className='my-6 px-4 space-y-6'>
-        <View className='justify-between items-start flex-row mb-6'>
-        <View>
+        <View className='my-6 px-4 '>
         <Text className='font-pmedium text-sm text-gray-100'>Search Result</Text>
         <Text className='text-2xl  font-psemibold text-white'>{query}</Text>
-        </View>
-        <View className='mt-1.5'>
 
-          <Image
-          source={icons.search}
-          className='w-9 h-10'
-          resizeMode='contain'
-          
-          />
-
+        <View className='mt-6 mb-8'>
+        <SearchInput  initialQuery={query}/>
 
         </View>
+
+
+
         
-        </View>
 
-        <SearchInput/>
+
         
         </View>
       )}
@@ -63,7 +59,7 @@ const Search = () => {
       ListEmptyComponent={()=>(
         <EmptyState
         title='No Videos  Found'
-        subtitle='No videos created yet'
+        subtitle='No videos found for this query'
         
         />
 
