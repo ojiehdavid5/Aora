2db2b6bb1eb4c29cc77c70c15b4eacf682b6d205
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-22 02:27:21 +0100

    done

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import FormField from '../../components/FormField'
 
 const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title:'',
+    video:null,
+    thumbnail:null,
+    prompt:''
+  })
   return (
-    <View>
-      <Text> Create</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView className='px-4 my-6'>
+      <Text className='text-2xl text-white font-psemibold'> Upload videos</Text>
+      <FormField
+      title='Video Title '
+      
+      />
+
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
