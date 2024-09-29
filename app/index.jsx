b7c8fdbb35ea4b8199ccceb8bcdd6e9a56906d2e
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-09-29 08:22:23 +0100

    done

--- app/index.jsx
+++ app/index.jsx
@@ -18,18 +18,18 @@ const RootLayout = () => {
    
 
         
-<SafeAreaView className="bg-primary h-full ">
+<SafeAreaView className="bg-black h-full ">
   <ScrollView contentContainerStyle={{height: '100%'}}>
     <View className='w-full items-center  h-full px-4'>
-<Image 
+{/* <Image 
 source={images.logo}
 className="w-[130px] h-[84px]"
 resizeMode='contain'
 
-/>
+/> */}
 <Image
-source={images.cards}
-className="max-w-[380px] w-full h-[300px]"
+source={images.primal}
+className="max-w-[380px] w-full h-[300px] rounded-sm"
 resizeMode='contain'
 
 />
