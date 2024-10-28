8c5c22cddaf72351cdcc150a8e4be8eb1e8edf04
ojieh (chuks) david <ojiehdavid5@gmail.com>
2024-10-28 11:35:55 +0100

    Update GlobalProvider.js

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -8,6 +8,9 @@ export const useGlobalContext=()=>useContext(GlobalContext)
 
 
   const GlobalProvider=({children})=>{
+
+
+
     const [isLoggedIn,setIsLoggedIn]=useState(false);
     const [user,setUser]=useState(null);
     const [isLoading, setIsLoading] = useState(true)
